b3c665c6b78182228fa6cb385a08e8958f690649
kuwamura <naoyan418@gmail.com>
2020-06-26 17:49:59 +0900

    first commit

---
+++ backend/app/Http/Controllers/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BookRequest;
+use App\Http\Controllers\Controller;
+use App\Book;
+
+class BookController extends Controller
+{
+    public function index(){
+    	$books = Book::all();
+
+    	return view('book/index', compact('books'));
+    }
+
+    public function edit($id) {
+    	$book = Book::findOrFail($id);
+
+    	return view('book/edit', compact('book'));
+    }
+
+    public function update(BookRequest $request, $id) 
+    {
+        $book = Book::findOrFail($id);
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect("/book");
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return redirect("/book");
+    }
+
+    public function create() {
+        $book = new Book();
+
+        return view('book/create', compact('book'));
+    }
+
+    public function store(BookRequest $request)
+    {
+        $book = new Book();
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+
+        return redirect("/book");
+    }
+}
